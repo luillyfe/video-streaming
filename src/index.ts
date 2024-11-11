26180cfabfd082dae71ae7dd56ebda89cbf1620e
Fermin Blanco <luillyfe89@gmail.com>
2024-11-11 11:07:42 -0500

    chore: adding security headers for better protection.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 // Import fastify
 import Fastify from "fastify";
+import helmet from "@fastify/helmet"
+
 // Import routes
 import mainRoute from "./routes";
 import config from "./config";
 
 // Configure logger
 const fastify = Fastify({ logger: true })
+
+// Basic security headers will be set
+fastify.register(helmet)
 
 // Use the register API to configure a new route
 fastify.register(mainRoute)
@@ -20,3 +25,7 @@ fastify.listen({ port: config.server.port, host: config.server.host }, (err, add
 
     fastify.log.info(`Server Running on ${address}`)
 })
+
+// https://github.com/fastify/fastify-helmet
+// This could cause trouble if some routes has not properky defined their MIME types.
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
--- src/routes.ts
+++ src/routes.ts
@@ -15,8 +15,21 @@ async function configureRoutes(fastify: FastifyInstance) {
     })
 
     // Main route
-    fastify.get('/', async () => {
-        return fs.createReadStream("./index.html")
+    fastify.get('/', {
+        helmet: {
+            // Let's weaken our CSP protection 👺👺👺 for fun
+            // Enable csp nonces generation with default content-security-policy option
+            enableCSPNonces: true
+        }
+    }, async (_, reply: FastifyReply) => {
+        reply.header('Content-Type', 'text/html'); // Explicitly set the Content-Type header
+
+        // Read the HTML file content
+        let html = await fs.promises.readFile("./index.html", 'utf-8');
+        html = html.replace(/<script>/g, `<script nonce="${reply.cspNonce.script}">`)
+        html = html.replace(/<style>/g, `<style nonce="${reply.cspNonce.style}">`)
+
+        return reply.send(html)
     })
 
     // Video Streaming
@@ -34,6 +47,15 @@ export default configureRoutes
 // https://fastify.dev/docs/latest/Guides/Getting-Started/
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Server-Timing
 // https://fastify.dev/docs/v5.1.x/Reference/Errors/#errors-in-fastify-lifecycle-hooks-and-a-custom-error-handler
+// https://github.com/fastify/fastify-helmet?tab=readme-ov-file#example---fastifyhelmet-configuration-using-the-helmet-shorthand-route-option
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/nonce
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src
+// https://github.com/fastify/fastify-helmet?tab=readme-ov-file#content-security-policy-nonce
+
+// Note From Mozilla Foundation: Only use nonce for cases where you have no way around using unsafe inline script 
+// or style contents. If you don't need nonce, don't use it. If your script is static, 
+// you could also use a CSP hash instead. (See usage notes on unsafe inline script.) 
+// Always try to take full advantage of CSP protections and avoid nonces or unsafe inline scripts whenever possible.
 
 // Range header
 // bytes 19005440-19051281/19051282
